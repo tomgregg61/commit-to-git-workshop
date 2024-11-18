0ba16075c8db14d65703350edd848d0c88e41d72
jemorak <jemima.orakwue@hotmail.co.uk>
2024-11-18 23:12:47 +0000

    Add initial code

---
+++ code/sample-code.js
@@ -0,0 +1,7 @@
+// This is a sample JavaScript file for demonstration purposes.
+
+function sayHello() {
+    console.log("Hello, world!");
+}
+
+// Change this function's message in Task 2 or Task 4.
